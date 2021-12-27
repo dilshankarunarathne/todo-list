fdcea7075c35880b7c93f65a64e6023fa32c93a7
Speed Coders <95556455+speed-coders@users.noreply.github.com>
2021-12-27 14:34:48 +0530

    initial commit

---
+++ src/com/example/toDoList/Main.java
@@ -0,0 +1,51 @@
+package com.example.toDoList;
+
+/**
+ * @author Sean M. Wader
+ * All rights reserved (C) zeeka LLC.
+ * 2020 November
+ * Kandy, Sri Lanka
+ */
+
+import com.example.toDoList.datamodel.TodoData;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mainwindow.fxml"));
+        primaryStage.setTitle("ToDo List");
+        primaryStage.setScene(new Scene(root, 720, 480));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try {
+            TodoData.getInstance().storeTodoItems();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void init() throws Exception {
+        try {
+            TodoData.getInstance().loadTodoItems();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/com/example/toDoList/datamodel/TodoItem.java
@@ -0,0 +1,52 @@
+package com.example.toDoList.datamodel;
+
+import java.time.LocalDate;
+
+/**
+ * @author Sean M. Wader
+ * All rights reserved (C) zeeka LLC.
+ * 2020 November
+ * Kandy, Sri Lanka
+ */
+
+public class TodoItem {
+
+    private String shortDescription;
+    private String details;
+    private LocalDate deadline;
+
+    public TodoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+//    @Override
+//    public String toString() {
+//        return shortDescription;
+//    }
+}
